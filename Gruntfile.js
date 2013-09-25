0176a564f0cfeb3d9bb09d95febf75b8acf15faf
Dave Cadwallader <dcadwallader@gmail.com>
2013-09-25 14:01:54 -0600

    Integration with SauceLabs for cross-browser testing, and first pass at beautification support to ease whitespace merge issues.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,78 @@
-module.exports = function ( grunt ) {
+module.exports = function(grunt) {
+
+    var browsers = [
+        {
+            browserName: "firefox",
+            version: "22",
+            platform: "XP"
+        },
+        {
+            browserName: "firefox",
+            version: "21",
+            platform: "XP"
+        },        
+        {
+            browserName: "chrome",
+            version: "29",
+            platform: "XP"
+        },
+        {
+            browserName: "chrome",
+            version: "28",
+            platform: "Mac 10.6"
+        },        
+        {
+            browserName: 'internet explorer',
+            platform: 'WIN8',
+            version: '10'
+        }, 
+        {
+            browserName: 'internet explorer',
+            platform: 'VISTA',
+            version: '9'
+        }, 
+        {
+            browserName: 'internet explorer',
+            platform: 'XP',
+            version: '8'
+        },
+        {
+            browserName: 'safari',
+            platform: 'Mac 10.8',
+            version: '6'
+        },
+        {
+            browserName: 'safari',
+            platform: 'Mac 10.6',
+            version: '5'
+        },
+        {
+            browserName: 'iphone',
+            platform: 'Mac 10.8',
+            version: '6'
+        },
+        {
+            browserName: 'iphone',
+            platform: 'Mac 10.8',
+            version: '5.1'
+        },
+        {
+            browserName: 'ipad',
+            platform: 'Mac 10.8',
+            version: '6'
+        },
+        {
+            browserName: 'ipad',
+            platform: 'Mac 10.8',
+            version: '5.1'
+        }
+            
+            
+];
+    
     var pkgConfig = grunt.file.readJSON('package.json');
     pkgConfig.version = grunt.file.readJSON('version.json').version;
-    grunt.initConfig( {
+    grunt.initConfig({
 
         pkg: pkgConfig,
         uglify: {
@@ -18,7 +89,7 @@ module.exports = function ( grunt ) {
         },
 
         blanket_mocha: {
-            all: [ 'specs/index.html' ],
+            all: ['specs/index.html'],
             options: {
                 threshold: 98,
                 log: true,
@@ -26,47 +97,69 @@ module.exports = function ( grunt ) {
                 mocha: {}
             }
         },
-        version       : {
-            defaults : {
-                src : [
+        version: {
+            defaults: {
+                src: [
                     'package.json', 'bower.json', '<%= pkg.name %>.js'
                 ]
             }
         },
-		connect    : {
-			options : {
-				port     : 9001,
-				hostname : 'localhost',
-				base     : '.'
-			},
-			test    : {}
-		},
-		concurrent : {
-			options        : {
-				logConcurrentOutput : true
-			},
-			test : [
-				'connect:test:keepalive', 'open:test'
-			]
-		},
-		open       : {
-			test : {
-				path : 'http://localhost:9001/specs/index.html'
-			}
-		}
-	} );
+        "jsbeautifier": {
+            files: ['beautify.json', 'Gruntfile.js', 'backbone.geppetto.js', 'specs/*.js'],
+            options: {
+                config: "beautify.json"
+            }
+        },
+        connect: {
+            options: {
+                port: 9001,
+                hostname: 'localhost',
+                base: '.'
+            },
+            test: {}
+        },
+        concurrent: {
+            options: {
+                logConcurrentOutput: true
+            },
+            test: [
+                'connect:test:keepalive', 'open:test'
+            ]
+        },
+        open: {
+            test: {
+                path: 'http://localhost:9001/specs/index.html'
+            }
+        },
+        'saucelabs-mocha': {
+            all: {
+                options: {
+                    urls: ["http://localhost:9001/specs/index.html"],
+                    build: process.env.TRAVIS_JOB_ID,
+                    concurrency: 3,
+                    browsers: browsers,
+                    testname: "Backbone.Geppetto",
+                    tags: ["master"]
+                }
+            }
+        }
+    });
 
-    grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-    grunt.loadNpmTasks( 'grunt-blanket-mocha' );
-    grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-    grunt.loadNpmTasks( 'grunt-version' );
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-concurrent');
-	grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-jsbeautifier');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-blanket-mocha');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-version');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-saucelabs');
+    
+    grunt.registerTask('beautify', ['jsbeautifier']);
+    grunt.registerTask('lint', ['jshint']);
+    grunt.registerTask('coverage', ['blanket_mocha']);
+    grunt.registerTask('travis', ['jshint', 'blanket_mocha', "saucelabs-mocha"]);
+    grunt.registerTask("sauce", ["connect", "saucelabs-mocha"]);
 
-    grunt.registerTask( 'lint', ['jshint'] );
-    grunt.registerTask( 'coverage', ['blanket_mocha'] );
-    grunt.registerTask( 'travis', ['jshint', 'blanket_mocha'] );
-
-    grunt.registerTask( 'default', [ 'version', 'uglify', 'jshint', 'blanket_mocha'] );
+    grunt.registerTask('default', ['version', 'uglify', 'jshint', 'blanket_mocha']);
 };
